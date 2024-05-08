66a0ca65e1236a19d50e647a7065c74509140778
Hamid-ben98 <78178773+Hamid-ben98@users.noreply.github.com>
2024-05-08 11:53:12 +0100

    mutashabis-groups endpoints
    Create grp
    Get
    Delete

---
+++ app/Http/Controllers/MutashabihsGroupController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Validator;
+use Illuminate\Http\Request;
+use App\Models\MutashabihsGroup;
+
+class MutashabihsGroupController extends Controller
+{
+    public function get ($id=null){
+
+        if ($id){
+            return MutashabihsGroup::find($id)->toJson(JSON_PRETTY_PRINT);
+        }
+        return MutashabihsGroup::all()->toJson(JSON_PRETTY_PRINT);
+    }
+
+    public function create (Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:mutashabihs_groups|max:50',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => [$validator->errors()->first('name')],
+            ],404);       
+        }
+
+        $group= MutashabihsGroup::create($request->all());
+
+        return $group->toJson(JSON_PRETTY_PRINT,200);
+
+    }
+    public function delete ($id){
+
+        $group = MutashabihsGroup::find($id);
+        if (!$group)  return response()->json([
+                'message' => 'This group don\'t exist',
+                 ],404);
+
+        $group->delete();
+        return response()->json([
+                'message' => 'Deleted with success',
+            ],200);
+   
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MutashabihsGroupController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group( function () {
+    
+    Route::get('mutashabihs_groups', [MutashabihsGroupController::class,'get']);
+    Route::get('mutashabihs_groups/{id}', [MutashabihsGroupController::class,'get']);
+    Route::post('mutashabihs_groups', [MutashabihsGroupController::class,'create']);
+    Route::DELETE('mutashabihs_groups/{id}', [MutashabihsGroupController::class,'delete']);
+});
 
